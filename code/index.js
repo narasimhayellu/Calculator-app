e9648760b32bc6c0479ba27eaf0228b786546b1e
narasimhayellu <narasimhayellu99@gmail.com>
2025-03-10 11:55:31 +0530

    updated files

---
+++ code/index.js
@@ -0,0 +1,40 @@
+
+// const box = document.querySelector("p");
+
+document.getElementById("top").addEventListener("click",function(){
+    document.innerHTML = "text"
+    console.log("submitted");
+   
+})
+
+
+// box.innerHTML = "post";
+// demo.innerHTML = "hello";
+// demo.style["color"] ="blue";
+
+// function colorChange(){
+//  demo.style["color"] ="red";
+//  console.log("web");
+// }
+// colorChange();
+
+// // demo.id = "red";
+// // red.innerHTML = "delete";
+
+// class post{
+//     constructor(age,name){
+//       this.age = age;
+//       this.name = name;
+//     }
+//     function(address){
+//   this.address = add;
+//     }
+//     details(){
+//       console.log(`box:${this.age}`);
+//     }
+//   }
+  
+//   let person = new post(22,'ted');
+
+//   person.details();
+
